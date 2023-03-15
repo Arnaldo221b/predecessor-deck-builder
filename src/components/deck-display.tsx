5e52c9e47e3f8637b4038fbd37585db156ace1a0
ArnaldoVanta <arnaldovanta@MacBook-Pro-de-Arnaldo.local>
2023-03-15 10:56:01 -0300

    Add assets, images specifically

--- src/components/deck-display.tsx
+++ src/components/deck-display.tsx
@@ -1,13 +1,19 @@
 import { useDeckStatusContext } from "../contexts/DeckBuilderContext";
 import Item from "./item";
+import GideonAvatarPic from '../assets/images/Gideon.webp'
 
 const DeckDisplay = () => {
     const { character, items, id } = useDeckStatusContext()
+
+    const avatarStyle = {
+        maxWidth: "72px"
+    }
 
     return (
         <>
             <div className='current-deck-section'>
                 <h2>Character : { character }</h2>
+                <img alt="character avatar" style={avatarStyle} src={GideonAvatarPic} ></img>
                 <h3>Qty added cards : { items.length }</h3>
                 <h4>Added Cards</h4>
                 <ul>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 const PredHome = () => {
     return (<>
         <h1>Welcome to Predecessor Deck Builder</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/JHpJZ6okj3A" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
     </>)
 }
 
